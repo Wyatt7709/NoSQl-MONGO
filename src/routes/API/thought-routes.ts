91c8e23a8a125ae240a584fe744ea60e21af74e0
Wyatt7709 <wwoydziak.7704@gmail.com>
2024-12-07 16:16:37 -0600

    File Recovered

---
+++ src/routes/API/thought-routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+const router = Router();
+
+import {
+    getThoughts,
+    createThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    getReactions,
+    createReaction,
+    deleteReaction,
+} from '../../controllers/thoughController.js';
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').get(getReactions).post(createReaction).delete(deleteReaction);
+
+export { router as thoughtRouter } ;
---
+++ src/routes/API/user-routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+const router = Router();
+
+import {
+    getUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    getFriends,
+    addFriend,
+    deleteFriend,
+} from '../../controllers/userContoller.js';
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends/:friendId').get(getFriends).post(addFriend).delete(deleteFriend);
+
+export { router as userRouter } ;
